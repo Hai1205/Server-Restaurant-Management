605e8205ec28e565bb801ccedaea7db1b38ea02e
Nguyen Hoang Hai <hainguyenhoang1205@gmail.com>
2025-01-12 21:25:11 +0700

    feat: add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-})
-@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGODB_URI');
+        try {
+          await mongoose.connect(uri);
+          console.log('Connected to MongoDB');
+        } catch (error) {
+          console.error('Error connecting to MongoDB:', error.message);
+          throw new Error('MongoDB connection failed!');
+        }
+        return {
+          uri,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
-// const dotenv = require('dotenv');
 
 async function bootstrap() {
-  // Load environment variables
-  dotenv.config({ path: './src/.env' });
-
-  // Extract environment variables
-  const MONGO = process.env.MONGO;
-  const PORT = process.env.PORT || 8080;
-
-  // Initialize the NestJS application
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
 
-  // Start the server
-  await app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  await app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
   });
-
-  // Connect to MongoDB using Mongoose
-  try {
-    await mongoose.connect(MONGO);
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1);
-  }
 }
 
 bootstrap();
