da843a84d3f78f5ac19cccb8b25c727d2e0f040e
Nguyen Hoang Hai <hainguyenhoang1205@gmail.com>
2025-01-14 12:07:09 +0700

    feat: add jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './passport/local-auth.quảd';
+import { JwtAuthGuard } from './passport/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async signIn(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.signIn(createAuthDto.email, createAuthDto.password);
+  handleLogin(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
 
 import { comparePasswordHandler } from '@/helpers/util';
+import { User } from '@/modules/users/schemas/user.schema';
 import { UsersService } from '@/modules/users/users.service';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { access } from 'fs';
 
 @Injectable()
 export class AuthService {
@@ -12,15 +12,18 @@ export class AuthService {
     private jwtService: JwtService
   ) { }
 
-  async signIn(email: string, pass: string): Promise<any> {
+  async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
-
     const isValidPassword = await comparePasswordHandler(pass, user.password);
-    if (!isValidPassword) {
-      throw new UnauthorizedException("Email or password is invalid");
-    }
-
+    
+    if (!user || !isValidPassword) return null;
+    return user;
+  }
+  
+  async login(user: any) {
     const payload = { sub: user._id, email: user.email }
-    return { access_token: await this.jwtService.signAsync(payload) }
+    const access_token = await this.jwtService.signAsync(payload);
+
+    return { access_token: access_token }
   }
 }
